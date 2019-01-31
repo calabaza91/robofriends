c641c32d2f4d61a2306c83beb362f2d23bcdc27d
Caleb Cox <calebcox2010@gmail.com>
2019-01-31 13:20:35 -0600

    Updates

---
+++ precache-manifest.718a86f5f7411822cd909709717f1bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "079708f36e39877e603d",
+    "url": "/static/js/main.079708f3.chunk.js"
+  },
+  {
+    "revision": "1e1729bc6944ba0178db",
+    "url": "/static/js/1.1e1729bc.chunk.js"
+  },
+  {
+    "revision": "079708f36e39877e603d",
+    "url": "/static/css/main.54bbcb8a.chunk.css"
+  },
+  {
+    "revision": "1e1729bc6944ba0178db",
+    "url": "/static/css/1.fee47740.chunk.css"
+  },
+  {
+    "revision": "fd55441e78f11bbf1e4f49450f6401a4",
+    "url": "/index.html"
+  }
+];
